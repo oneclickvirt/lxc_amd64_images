065bb50b76d857165ce7b0fc125435b7b91efde1
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-09-06 19:20:47 +0800

    Update clone_modify_yaml.sh

--- clone_modify_yaml.sh
+++ clone_modify_yaml.sh
@@ -124,7 +124,7 @@ insert_content_2=$(cat /home/runner/work/lxc_amd64_images/lxc_amd64_images/bash_
 line_number=$(($(wc -l < gentoo.yaml) - 3))
 head -n $line_number gentoo.yaml > temp.yaml
 echo "$insert_content_2" >> temp.yaml
-tail -n 7 gentoo.yaml >> temp.yaml
+tail -n 3 gentoo.yaml >> temp.yaml
 mv temp.yaml gentoo.yaml
 sed -i -e '/environment:/i \ ' gentoo.yaml
 sed -i 's/- default/- openrc/g' gentoo.yaml
